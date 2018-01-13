cf35c950135381664f9e7241f6cce5630a5e65a8
Daniil Goncharov <dangofinetime@mail.ru>
2018-01-13 18:01:15 +0300

    Добавили вывод расписаня в телеграм-бота

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 from models import Subject, Lesson, Class
 from db import db_session
+from datetime import datetime
+from collections import OrderedDict
 
 
 logging.basicConfig(format="%(name)s -%(levelname)s-%(message)s",
@@ -9,15 +11,30 @@ logging.basicConfig(format="%(name)s -%(levelname)s-%(message)s",
     filename="bot.log"
     )
 
+
 def lessons(bot, update):
     class_titles = db_session.query(Class.title).all()
     class_titles = [title[0] for title in class_titles]
-    print(class_titles)
     class_name = update.message.text [9:]
-    if class_name in class_titles:
-        update.message.reply_text(class_name)
-    else:
+    current_class = db_session.query(Class).filter(Class.title==class_name).first()
+    if  current_class is None :
         update.message.reply_text("Такого класса нету!")
+    else:
+        lessons = db_session.query(Lesson).filter(Lesson.cl_id==current_class.id).order_by(Lesson.day_of_week, Lesson.time).all()
+        subjects = db_session.query(Subject).all()
+        subjects_dict = {}
+        for subject in subjects:
+            subjects_dict[subject.id] = subject.title
+        lessons_by_day = OrderedDict()
+        for lesson in lessons:
+            if lesson.day_of_week not in lessons_by_day:
+                lessons_by_day[lesson.day_of_week] = []
+            lessons_by_day[lesson.day_of_week].append(lesson)
+        for key in lessons_by_day.keys():
+            table_text = '{}:\n\n'.format(key)
+            for lesson in lessons_by_day[key]:
+               table_text = table_text+'{} {} [{}]\n'.format(lesson.time.strftime("%H:%M"), lesson.title, subjects_dict[lesson.sub_id])
+               update.message.reply_text(table_text)
 
 
 def main():
@@ -26,4 +43,7 @@ def main():
     dp.add_handler(CommandHandler("lessons", lessons))
     updater.start_polling()
     updater.idle()
-main()
+
+
+if __name__ = '__main__':   
+    main()
--- init_data.py
+++ init_data.py
@@ -5,7 +5,7 @@ import datetime
 subject = db_session.query(Subject).first()
 
 if subject is None:
-	titles = ['Физика', 'Информатика', 'Алгебра', 'Английский язык']
+	titles = ['Физика', 'Информатика', 'Алгебра', 'Английский язык', 'Русский язык', 'Физ-ра', 'Геометрия', 'Химия']
 	for title in titles:
 		db_session.add(Subject(title=title))
 	db_session.commit()
@@ -25,8 +25,13 @@ lesson = db_session.query(Lesson).first()
 if lesson is None:
 	lesson1 = Lesson(title='Первый урок', time=datetime.time(8, 30), day_of_week='Понедельник', sub_id=1, cl_id=1)
 	lesson2 = Lesson(title='Второй урок', time=datetime.time(9, 30), day_of_week='Вторник', sub_id=4, cl_id=2)
-	lesson3 = Lesson(title='Третий урок', time=datetime.time(10, 30), day_of_week='Среда', sub_id=5, cl_id=3)
-	db_session.add_all([lesson3, lesson2, lesson1])
+	lesson3 = Lesson(title='Третий урок', time=datetime.time(10, 30), day_of_week='Среда', sub_id=3, cl_id=3)
+	lesson4 = Lesson(title='Четвёртый урок', time=datetime.time(11, 30), day_of_week='Пятница', sub_id=8, cl_id=4)
+	lesson5 = Lesson(title='Пятый урок', time=datetime.time(12, 30), day_of_week='Среда', sub_id=5, cl_id=2)
+	lesson6 = Lesson(title='Шестой урок', time=datetime.time(13, 30), day_of_week='вторник', sub_id=1, cl_id=4)
+	lesson7 = Lesson(title='Первый урок', time=datetime.time(8, 30), day_of_week='Понедельник', sub_id=7, cl_id=3)
+	lesson8 = Lesson(title='Третий урок', time=datetime.time(10, 30), day_of_week='Четверг', sub_id=6, cl_id=2)
+	db_session.add_all([lesson3, lesson2, lesson1, lesson4, lesson5, lesson6, lesson7, lesson8])
 	db_session.commit()
 
 
